fb6705d7980cf725e2d079346e25fdf30f096767
Kat Webb <121592902+kwebb31@users.noreply.github.com>
2024-01-20 18:45:55 -0800

    Update bardRequest.py

--- bardRequest.py
+++ bardRequest.py
@@ -1,37 +1,31 @@
+import textwrap
 import os
-import requests
-import google.generativeai as palm
-import json
-from vertexai.preview import generative_models
-palm.configure(api_key=os.environ['BardSneakret'])
-#from vertexai.preview import generative_models
+from IPython.display import Markdown
+import google.generativeai as genai
 
-#your individual API Key goes where "BardSneakret" is. Do this by creating a secret on replit and putting your Bard API key in it.
 my_secret = os.environ['BardSneakret']
-palm.configure(api_key=my_secret)
+genai.configure(api_key=my_secret)
+#my_secret = os.environ['BardSneakret']
+#palm.configure(api_key=my_secret)
 
 def query(query):
-#discord can only return 2000 characters or less because of the message size limit on discord. So whatever the currently listed query is, ask it to do that in 2k characters or less
-  query+= "In 2000 characters or less, please"
+#discord can only return 2000 characters or less because of the message size limit on discord.
+  model = genai.GenerativeModel('gemini-pro')
+  query+= "The response absolutely must be 1900 characters in length or less"
+  responses = model.generate_content(query)
   
-  #list the generative model parameters
-  model = palm.GenerativeModel(model_name="gemini-pro",
-                                #generation_config=generation_config,
-                                 #safety_settings=safety_config
-                              )
-    
-  #uses the model, without typing out the characters one at a time, uses default safety settings, and establishes temperature (the amount of stuff the AI can make up)  
-  responses = model.generate_content(
-                                     [query],
-                                     stream=False,
-                                     #safety_settings=safety_config,
-                                     generation_config={"max_output_tokens": 2048, "temperature": 1.0} )
-
-  #create a list of all of the strings Bard is sending over. print each to console so the admin can see what the bot is doing. append it to a response 
+  
   text_responses = []
   for response in responses:
       print(response.text)
       text_responses.append(response.text)
   #return all of the list appended together to make one string, to the function that called it in the main function.
-  return "".join(text_responses)
+  if(len(text_responses) >= 1 ):
+    return "".join(text_responses)
+  else:
+    return "Sorry, I could not find an answer to your question."
+  
+#def to_markdown(text):
+#  text = text.replace('•', '  *')
+#  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
  
